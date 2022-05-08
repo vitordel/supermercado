1ee6f82c713bd44bcfa9bbcdef76d45a304cde6c
Vitor Delgado <vitor.ldelgado@gmail.com>
2022-05-08 09:38:05 -0300

    Add Tipo

---
+++ src/Tipo.java
@@ -0,0 +1,21 @@
+public enum Tipo {
+
+    ALIMENTO (1.2),
+    BEBIDA(2.3),
+    HIGIENE(1.5);
+
+
+    private final double markup;
+    Tipo(double markup){
+
+        this.markup = markup;
+    }
+
+    public double getMarkup() {
+        return markup;
+    }
+
+    public static double calcularVenda(Tipo tipo, double custo){
+        return tipo.getMarkup() * custo;
+    }
+}
