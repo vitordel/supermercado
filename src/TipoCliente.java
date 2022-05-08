ba0dc7670db9187e3bf0faa02f98622b0a2caa19
Vitor Delgado <vitor.ldelgado@gmail.com>
2022-05-08 09:38:35 -0300

    Add TipoCliente

---
+++ src/TipoCliente.java
@@ -0,0 +1,18 @@
+public enum TipoCliente {
+    PF (1),
+    PJ (0.95),
+    VIP (0.85);
+
+    private final double desconto;
+
+    TipoCliente (double desconto){
+        this.desconto = desconto;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
+    public static double calcularDesconto(TipoCliente tipoCliente, double valorTotal){
+        return (tipoCliente.getDesconto() * valorTotal);
+    }
+}
